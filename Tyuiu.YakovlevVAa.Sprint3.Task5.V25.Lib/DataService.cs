1642dc37a944855b29db98aad63d9ab7aed04d9c
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-11-01 21:29:34 +0500

    .

--- Tyuiu.YakovlevVAa.Sprint3.Task5.V25.Lib/DataService.cs
+++ Tyuiu.YakovlevVAa.Sprint3.Task5.V25.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.YakovlevVAa.Sprint3.Task5.V25.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             x = 2;
-            double y = 1.0;
+            double y = 0;
             for (int i = startValue1; i <= stopValue1; i++)
             {
                 for (int k = startValue2; k <= stopValue2; k++)
